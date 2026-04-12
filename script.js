8974e983b3ddde491cbb927a66355e9da97012cc
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-04-12 14:06:42 +0300

    script.js

--- script.js
+++ script.js
@@ -31,3 +31,45 @@ steps[current].classList.add("active");
 });
 
 });
+
+
+const countries = [
+"Argentina","Australia","Austria","Belgium","Brazil","Canada","China",
+"Denmark","Finland","France","Germany","India","Italy","Japan",
+"Kenya","Mexico","Netherlands","Norway","Portugal","Qatar",
+"Saudi Arabia","Singapore","South Africa","Spain","Sweden",
+"Switzerland","UAE","United Kingdom","United States"
+];
+
+const countrySelect = document.getElementById("country");
+
+countries.sort().forEach(country=>{
+let option = document.createElement("option");
+option.value = country;
+option.textContent = country;
+countrySelect.appendChild(option);
+});
+
+
+const rates = {
+USD:1,
+EUR:1.08,
+GBP:1.25,
+KES:0.0078
+};
+
+const budgetInput = document.getElementById("budgetLocal");
+const currencySelect = document.getElementById("currency");
+const preview = document.querySelector(".usdPreview");
+
+function convert(){
+let amount = budgetInput.value;
+let currency = currencySelect.value;
+
+let usd = amount * rates[currency];
+
+preview.innerText = "≈ $" + usd.toFixed(2) + " USD";
+}
+
+budgetInput.addEventListener("input",convert);
+currencySelect.addEventListener("change",convert);
