6120868106833ff647592784a443a78a15a4648b
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-04-30 15:35:41 +0300

    script.js

--- script.js
+++ script.js
@@ -119,3 +119,18 @@ e.preventDefault();
 
 window.location.href = "success.html";
 });
+
+// HAMBURGER MENU
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+menu.classList.toggle("active");
+});
+
+document.querySelectorAll(".menu-overlay a").forEach(link=>{
+link.addEventListener("click",()=>{
+menu.classList.remove("active");
+});
+});
